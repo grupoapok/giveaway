c62f493bc3e58474a23cf7aa0d7bcc9a71cfc340
Jose Carrero <josercl@gmail.com>
2019-09-20 16:13:14 -0400

    locale basado en dominio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', request()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -29,6 +29,6 @@
         });
     };
 </script>
-<script async defer src="https://connect.facebook.net/{{ str_replace('_', '-', request()->getLocale()) }}/sdk.js"></script>
+<script async defer src="https://connect.facebook.net/{{ str_replace('_', '-', app()->getLocale()) }}/sdk.js"></script>
 </body>
 </html>
