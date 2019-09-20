0e336f110b8d7890eec8d5981bd652fffddfc925
Jose Carrero <josercl@gmail.com>
2019-09-20 16:15:20 -0400

    .

--- app/Http/Controllers/LangController.php
+++ app/Http/Controllers/LangController.php
@@ -10,12 +10,12 @@ class LangController extends Controller {
     use ProcessTokenTrait;
 
     function index($step, Request $request) {
-        return  __($step, [], $request->getLocale());
+        return  __($step, [], app()->getLocale());
     }
 
     function task(Task $task, Request $request) {
         $arr = [];
-        $arr[$task->type]  = __("tasks.".$task->type, ["tickets" => $task->tickets], $request->getLocale());
+        $arr[$task->type]  = __("tasks.".$task->type, ["tickets" => $task->tickets], app()->getLocale());
         return $arr;
     }
 }
