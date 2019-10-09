2de7a31595bf8537a927b9256b7e188b5c22829d
Dev Account <dev@grupoapok.com>
2019-10-09 18:14:14 +0000

    Fixes on texto

--- resources/lang/es/share.php
+++ resources/lang/es/share.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    "twitter" => "¡Gana una Página Web para tu Negocio! @grupoapok está sorteando una página web y para participar solo debes dar clic en el enlace. ¡Es muy fácil!",
-    "facebook" => "¡Participa y gana Una Página Web para tu marca o negocio!\n\nApok está regalando una página web profesional. Ya yo estoy participando y tú también puedes hacerlo haciendo clic en el enlace.",
+    "twitter" => "¡Gana una Página Web para tu Negocio! @grupoapok está sorteando una página web y para participar solo debes entrar en https://sorteo.grupoapok.com . ¡Es muy fácil!",
+    "facebook" => "¡Participa y gana Una Página Web para tu marca o negocio!\n\nApok está regalando una página web profesional. Ya yo estoy participando y tú también puedes hacerlo haciendo entrando en https://sorteo.grupoapok.com .",
     "linkedin" => "Una Página Web es imprescindible para tu Empresa o Marca Personal.\n\nApok está sorteando una Página Web Profesional y es tu oportunidad de obtener una. Puedes participar en SIMPLES PASOS.\n\n¡Ingresa a http://sorteo.grupoapok.com y Participa!"
 ];
